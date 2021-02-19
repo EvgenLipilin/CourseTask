99cd00599044bfbe9020f3ff93860e1dee05d75c
EvgenLipilin <evgenlipilin@yandex.ru>
2021-02-19 16:29:15 +0300

    Добавление Моделей структуры

---
+++ Курсовая/Course2FinalTask/Code/Model/CurrentUser.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentUser.swift
+//  Course2FinalTask
+//
+//  Created by Евгений on 19.02.2021.
+//  Copyright © 2021 e-Legion. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentUser: Codable {
+    var id: String
+    var username: String
+    var fullName: String
+    var avatar: String
+    var currentUserFollowsThisUser: Bool
+    var currentUserIsFollowedByThisUser: Bool
+    var followsCount: Int
+    var followedByCount: Int
+}
--- Курсовая/Course2FinalTask/Code/Model/Posts.swift
+++ Курсовая/Course2FinalTask/Code/Model/Posts.swift
@@ -8,37 +8,14 @@
 
 import Foundation
 
-let posts = DataProviders.shared.postsDataProvider
-
-
-class Posts: PostsDataProviderProtocol {
-    func feed(queue: DispatchQueue?, handler: @escaping ([Post]?) -> Void) {
-        posts.feed(queue: queue, handler: handler)
-        
-    }
-    
-    func post(with postID: Post.Identifier, queue: DispatchQueue?, handler: @escaping (Post?) -> Void) {
-        posts.post(with: postID,queue: queue,handler: handler)
-    }
-    
-    func findPosts(by authorID: User.Identifier, queue: DispatchQueue?, handler: @escaping ([Post]?) -> Void) {
-        posts.findPosts(by: authorID,queue: queue,handler: handler)
-    }
-    
-    func likePost(with postID: Post.Identifier, queue: DispatchQueue?, handler: @escaping (Post?) -> Void) {
-        posts.likePost(with: postID,queue: queue,handler: handler)
-    }
-    
-    func unlikePost(with postID: Post.Identifier, queue: DispatchQueue?, handler: @escaping (Post?) -> Void) {
-        posts.unlikePost(with: postID,queue: queue,handler: handler)
-    }
-    
-    func usersLikedPost(with postID: Post.Identifier, queue: DispatchQueue?, handler: @escaping ([User]?) -> Void) {
-        posts.usersLikedPost(with: postID,queue: queue,handler: handler)
-    }
-    
-    func newPost(with image: UIImage, description: String, queue: DispatchQueue?, handler: @escaping (Post?) -> Void) {
-        posts.newPost(with: image, description: description, queue: queue, handler: handler)
-    }
+struct Post: Codable {
+    var id: String
+    var description: String
+    var image: String
+    var createdTime: Date
+    var currentUserLikesThisPost: Bool
+    var likedByCount: Int
+    var author: String
+    var authorUsername: String
+    var authorAvatar: String
 }
-
