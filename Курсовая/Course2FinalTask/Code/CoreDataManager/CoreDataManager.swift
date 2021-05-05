35b58b2b0e287d18373d166c84a8659a7fc98bed
Евгений Липилин <>
2021-05-05 20:16:24 +0300

    KeychaimManager добавление

--- Курсовая/Course2FinalTask/Code/CoreDataManager/CoreDataManager.swift
+++ Курсовая/Course2FinalTask/Code/CoreDataManager/CoreDataManager.swift
@@ -10,4 +10,20 @@ import Foundation
 import UIKit
 import CoreData
 
+protocol CoreDataProtocol {
+    func getContext() -> NSManagedObjectContext
+    func save(context: NSManagedObjectContext)
+    func createObject<T: NSManagedObject>(from entity: T.Type) -> T
+    func delete(object: NSManagedObject)
+    func fetchData<T: NSManagedObject>(for entity: T.Type) -> [T]
+}
+
+protocol CoreDataInstagram {
+    func saveFeedInCoreData(for entity: Feed.Type, posts: [Post])
+    func saveCurrentUserInCoreData(for entity: CurrentUser.Type, user: User)
+    func fetchFeed(for entity: Feed.Type) -> [Post]
+    func fetchCurrentUser(for entity: CurrentUser.Type) -> [User]
+    func deleteAllObjects(objects: [Feed])
+    func deleteCurrentUser()
+}
 
